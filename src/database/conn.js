fa2cd369ba3054dce879fe42e6feec39c6d80376
Marin <kuglermarinkevin@gmail.com>
2024-09-16 11:19:48 -0300

    init: primeiro commit

---
+++ src/database/conn.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const mongoBD = "";
+const database = "lionsbet";
+
+const main = async () => {
+    try {
+        await mongoose.connect
+    } catch (error) {
+        
+    }
+}
---
+++ src/database/models/user_model.js
@@ -0,0 +1,5 @@
+import conn from "../conn.js";
+
+const Schema = conn.Schema
+
+const walletSchema
